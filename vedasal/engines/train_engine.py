3c5588bf12059af5bd7bc779fd5f9dc0b2901cb2
shuhanzhang <shuhan.zhang@media-smart.cn>
2022-01-26 14:21:56 +0800

    implement modules

--- vedasal/engines/train_engine.py
+++ vedasal/engines/train_engine.py
@@ -1,3 +1,4 @@
+import torch
 from vedacore.optimizers import build_optimizer
 from vedacore.misc import registry
 
@@ -7,10 +8,11 @@ from .base_engine import BaseEngine
 
 @registry.register_module('engine')
 class TrainEngine(BaseEngine):
-    def __init__(self, model, criterion, optimizer):
+    def __init__(self, model, criterion, optimizer, split_size=None):
         super().__init__(model)
         self.criterion = build_criterion(criterion)
         self.optimizer = build_optimizer(self.model, optimizer)
+        self.split_size = split_size
 
     def extract_maps(self, frames, hidden_state=None):
         feat, hidden_state = self.model(frames, hidden_state, train=True)
@@ -20,6 +22,17 @@ class TrainEngine(BaseEngine):
         return self.forward_impl(**data)
 
     def forward_impl(self, frames, maps, frame_metas):
-        feat, hidden_state = self.extract_maps(frames)
-        losses = self.criterion.loss(feat, maps)
+        if self.split_size:
+            frames = torch.split(frames, self.split_size, dim=1)
+            hidden_state = None
+            preds = []
+            for split in frames:
+                split_preds, hidden_state = self.extract_maps(split,
+                                                              hidden_state)
+                preds.append(split_preds)
+            preds = torch.cat(preds, dim=1)
+        else:
+            preds, hidden_state = self.extract_maps(frames)
+        losses = self.criterion.loss(preds, maps)
+
         return losses
